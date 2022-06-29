89800ccfa3bb640faec435860b495bc0c21bff4f
Mariano Wiñar <50705701+winarmarianoj@users.noreply.github.com>
2022-06-28 23:10:44 -0300

    Iteration two

--- src/test/kotlin/FizzBuzz.kt
+++ src/test/kotlin/FizzBuzz.kt
@@ -1,13 +1,46 @@
 class FizzBuzz() {
-    fun GetListFizzBuzz(): Array<String?> {
-        val arrayFizzBuzz = arrayOfNulls<String>(100)
+
+    private val FIZZ: String = "Fizz"
+
+    fun GetListFizzBuzz(): Set<String> {
+        /*val arrayFizzBuzz = arrayOfNulls<String>(100)
         var index = 1
         while (index <= arrayFizzBuzz.size) {
             arrayFizzBuzz[index-1] = index.toString()
             index++
         }
+        return arrayFizzBuzz*/
 
-        return arrayFizzBuzz
+
+        return GetArray().toSet()
+
+    }
+
+    private fun GetArray(): MutableList<String> {
+        var list = mutableListOf<String>()
+        /*for (i in 1..100){
+            if(i % 3 == 0) {
+                list.add(FIZZ)
+            }else{
+                list.add(i.toString())
+            }
+        }*/
+
+        var index = 1
+        while (index <= 100) {
+            if(index % 3 == 0) {
+                list.add("Fizz")
+            }else{
+                list.add(index.toString())
+            }
+            index++
+        }
+
+        /*println(list)
+        println(list.indexOf("30"))
+        println(list.elementAt(29))*/
+
+        return list
     }
 
 }
--- src/test/kotlin/FizzBuzzShould.kt
+++ src/test/kotlin/FizzBuzzShould.kt
@@ -1,24 +1,43 @@
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import kotlin.test.assertTrue
 
 
 class FizzBuzzShould {
+
+    private var fizzBuzz = FizzBuzz()
+
+    @BeforeEach
+    fun Setup(){
+
+    }
+
     @Test
     fun `Create an Array`(){
-        val fizzBuzz = FizzBuzz()
         val result = fizzBuzz.GetListFizzBuzz()
         assertThat(99).isEqualTo(result.size)
     }
 
     @Test
     fun `Return all numbers on String`(){
-        val fizzBuzz = FizzBuzz()
         val result = fizzBuzz.GetListFizzBuzz()
         //val expectedNumbers = Array(99){(it + 1).rangeTo(1)}
-        assertThat(result[result.size-1]).isEqualTo("100")
-        assertThat(result[0]).isEqualTo("1")
+        assertThat(result.elementAt(result.size-1)).isEqualTo("100")
+        assertThat(result.first()).isEqualTo("1")
+
+
+    }
+
+    @Test
+    fun `Return Fizz when number divisible by three`(){
+
+        val result = fizzBuzz.GetListFizzBuzz()
+
+        assertThat(result.elementAt(2)).isEqualTo("Fizz")
+        assertThat(result.elementAt(29)).isEqualTo("Fizz")
     }
 
 
 }
+
