9a9c41b6024ca11faac116611a477a3f5a7ee0b9
Mariano Wiñar <50705701+winarmarianoj@users.noreply.github.com>
2022-06-30 21:28:24 -0300

    Finished

--- src/test/kotlin/FizzBuzz.kt
+++ src/test/kotlin/FizzBuzz.kt
@@ -1,46 +1,25 @@
 class FizzBuzz() {
 
-    private val FIZZ: String = "Fizz"
-
-    fun GetListFizzBuzz(): Set<String> {
-        /*val arrayFizzBuzz = arrayOfNulls<String>(100)
-        var index = 1
-        while (index <= arrayFizzBuzz.size) {
-            arrayFizzBuzz[index-1] = index.toString()
-            index++
+    fun GetListFizzBuzz(): List<String> {
+        var list = mutableListOf<String>()
+        for (index in 1..100) {
+            list.add(verifyNumber(index))
         }
-        return arrayFizzBuzz*/
-
-
-        return GetArray().toSet()
-
+        return list
     }
 
-    private fun GetArray(): MutableList<String> {
-        var list = mutableListOf<String>()
-        /*for (i in 1..100){
-            if(i % 3 == 0) {
-                list.add(FIZZ)
-            }else{
-                list.add(i.toString())
-            }
-        }*/
-
-        var index = 1
-        while (index <= 100) {
-            if(index % 3 == 0) {
-                list.add("Fizz")
-            }else{
-                list.add(index.toString())
-            }
-            index++
+    private fun verifyNumber(index: Int): String {
+        var result = ""
+        if(index % 3 == 0 && index % 5 == 0){
+            result = "FizzBuzz"
+        }else if(index % 3 == 0) {
+            result = "Fizz"
+        }else if(index % 5 == 0){
+            result = "Buzz"
+        }else{
+            result = index.toString()
         }
-
-        /*println(list)
-        println(list.indexOf("30"))
-        println(list.elementAt(29))*/
-
-        return list
+        return result
     }
 
 }
--- src/test/kotlin/FizzBuzzShould.kt
+++ src/test/kotlin/FizzBuzzShould.kt
@@ -1,41 +1,47 @@
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import kotlin.test.assertTrue
 
 
 class FizzBuzzShould {
 
     private var fizzBuzz = FizzBuzz()
 
-    @BeforeEach
-    fun Setup(){
-
-    }
-
     @Test
     fun `Create an Array`(){
         val result = fizzBuzz.GetListFizzBuzz()
-        assertThat(99).isEqualTo(result.size)
+        assertThat(result.size).isEqualTo(100)
     }
 
     @Test
     fun `Return all numbers on String`(){
         val result = fizzBuzz.GetListFizzBuzz()
-        //val expectedNumbers = Array(99){(it + 1).rangeTo(1)}
-        assertThat(result.elementAt(result.size-1)).isEqualTo("100")
+        assertThat(result.elementAt(result.size-1)).isEqualTo("Buzz")
         assertThat(result.first()).isEqualTo("1")
-
-
     }
 
     @Test
     fun `Return Fizz when number divisible by three`(){
-
         val result = fizzBuzz.GetListFizzBuzz()
+        assertThat(result[2]).isEqualTo("Fizz")
+        assertThat(result.get(17)).isEqualTo("Fizz")
+        assertThat(result.get(92)).isEqualTo("Fizz")
+    }
 
-        assertThat(result.elementAt(2)).isEqualTo("Fizz")
-        assertThat(result.elementAt(29)).isEqualTo("Fizz")
+    @Test
+    fun `Return Buzz when number divisible by five`(){
+        val result = fizzBuzz.GetListFizzBuzz()
+        assertThat(result[4]).isEqualTo("Buzz")
+        assertThat(result.get(49)).isEqualTo("Buzz")
+        assertThat(result.get(99)).isEqualTo("Buzz")
+    }
+
+    @Test
+    fun `Return FizzBuzz when number divisible by three and five`(){
+        val result = fizzBuzz.GetListFizzBuzz()
+        assertThat(result[14]).isEqualTo("FizzBuzz")
+        assertThat(result.get(29)).isEqualTo("FizzBuzz")
+        assertThat(result.get(89)).isEqualTo("FizzBuzz")
     }
 
 
